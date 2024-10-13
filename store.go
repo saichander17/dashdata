8eea68b862684de4aadaf08cb4e900e369c26b3a
Sai Chander <saichander17@gmail.com>
2024-10-13 13:07:23 +0800

    Simple store in main package

--- store.go
+++ store.go
@@ -1,47 +1,35 @@
 package main
 
 import (
-	"hash/fnv"
-	"sync"
+        "sync"
 )
 
-const lockCount = 1024 // Number of locks, can be adjusted based on expected concurrency
-
 type Store struct {
-	data  map[string]string
-	locks [lockCount]sync.RWMutex
+        data map[string]string
+        mu   sync.RWMutex
 }
 
 func NewStore() *Store {
-	return &Store{
-		data: make(map[string]string),
-	}
-}
-
-func (s *Store) lockIndex(key string) uint32 {
-	h := fnv.New32a()
-	h.Write([]byte(key))
-	return h.Sum32() % lockCount
+        return &Store{
+                data: make(map[string]string),
+        }
 }
 
 func (s *Store) Set(key, value string) {
-	lockIndex := s.lockIndex(key)
-	s.locks[lockIndex].Lock()
-	defer s.locks[lockIndex].Unlock()
-	s.data[key] = value
+        s.mu.Lock()
+        defer s.mu.Unlock()
+        s.data[key] = value
 }
 
 func (s *Store) Get(key string) (string, bool) {
-	lockIndex := s.lockIndex(key)
-	s.locks[lockIndex].RLock()
-	defer s.locks[lockIndex].RUnlock()
-	value, exists := s.data[key]
-	return value, exists
+        s.mu.RLock()
+        defer s.mu.RUnlock()
+        value, exists := s.data[key]
+        return value, exists
 }
 
 func (s *Store) Delete(key string) {
-	lockIndex := s.lockIndex(key)
-	s.locks[lockIndex].Lock()
-	defer s.locks[lockIndex].Unlock()
-	delete(s.data, key)
+        s.mu.Lock()
+        defer s.mu.Unlock()
+        delete(s.data, key)
 }
